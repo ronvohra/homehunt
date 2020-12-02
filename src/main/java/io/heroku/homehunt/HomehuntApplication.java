7f942546b599a51dcfaf05d1bb35f037175d2317
Raunaq Vohra <vohrar@spotify.com>
2020-12-02 10:21:57 +0000

    Initial commit

---
+++ src/main/java/io/heroku/homehunt/HomehuntApplication.java
@@ -0,0 +1,13 @@
+package io.heroku.homehunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomehuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomehuntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/heroku/homehunt/HomehuntApplicationTests.java
@@ -0,0 +1,13 @@
+package io.heroku.homehunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomehuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
