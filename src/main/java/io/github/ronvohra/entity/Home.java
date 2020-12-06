f2bf76b93f35a577fc220e005c0c734c52779383
Raunaq Vohra <ronvohra@gmail.com>
2020-12-06 15:01:41 +0000

    Fix both tests by using H2 and generating IDs explicitly

--- src/main/java/io/github/ronvohra/entity/Home.java
+++ src/main/java/io/github/ronvohra/entity/Home.java
@@ -1,10 +1,13 @@
 package io.github.ronvohra.entity;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
-public class Home extends PanacheEntity {
+public class Home extends PanacheEntityBase {
   public String streetAddress;
   public String nearestStation;
   public String directCentralStation;
@@ -13,4 +16,8 @@ public class Home extends PanacheEntity {
   public int timeToCentral;
   public int numBedrooms;
   public int numBathrooms;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
 }
--- src/test/java/io/github/ronvohra/HomeResourceTest.java
+++ src/test/java/io/github/ronvohra/HomeResourceTest.java
@@ -1,81 +1,46 @@
 package io.github.ronvohra;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.hamcrest.Matchers.is;
 
 import io.github.ronvohra.entity.Home;
-import io.quarkus.panache.mock.PanacheMock;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusTest;
-import java.util.List;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 @QuarkusTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
 public class HomeResourceTest {
-
-  public static final String ADDRESS_YEMEN_ROAD = "14 Yemen Road, Yemen";
-  public static final String ADDRESS_BAKER_STREET = "221b Baker Street";
-
-  @BeforeAll
-  static void setUp() {
-    var home1 =
-        createHome(ADDRESS_YEMEN_ROAD, 2, 2, "station1", 5, "centralStation1", 10, "good shizz");
-    var home2 =
-        createHome(ADDRESS_BAKER_STREET, 3, 1, "Baker Street", 4, "Baker Street", 0, "elementary");
-    PanacheMock.mock(Home.class);
-    var mockedHomes = List.of(home1, home2);
-    // This is valid and required, since Mockito.when... doesn't work when you create mockedHomes
-    // as above (type conflict - List<Home> v/s the expected List<PanacheBaseEntity>).
-    PanacheMock.doReturn(mockedHomes).when(Home.class).listAll();
-    Mockito.when(Home.count()).thenReturn((long) mockedHomes.size());
-    PanacheMock.doNothing().when(Home.class).persist();
-  }
 
   @Test
   public void getHomesReturnsAllHomes() {
-    given()
-        .when()
-        .get("/homes")
-        .then()
-        .statusCode(200)
-        .body(containsString(ADDRESS_YEMEN_ROAD), containsString(ADDRESS_BAKER_STREET));
+    given().when().get("/homes").then().statusCode(200).body("size()", is(2));
   }
 
   @Test
   public void postHomesCreatesNewHome() {
-    var home = createHome("1600 Pennsylvania Ave", 100, 200, "wdc", 20, "wdc", 0, "White House");
-    var mockHome = Mockito.mock(Home.class, Mockito.RETURNS_DEEP_STUBS);
-    Mockito.when(mockHome.find(anyString(), anyString()).firstResult()).thenReturn(home);
+    var home = createHome();
     given()
-        .header("Content-Type","application/json" )
+        .header("Content-Type", "application/json")
         .body(home)
         .when()
         .post("/homes")
         .then()
         .statusCode(200)
-        .body(containsString("White House"));
+        .body("streetAddress", is(home.streetAddress));
   }
 
-  private static Home createHome(
-      String streetAddress,
-      int numBedrooms,
-      int numBathrooms,
-      String nearestStation,
-      int walkTimeToStation,
-      String directCentralStation,
-      int timeToCentral,
-      String notes) {
+  private static Home createHome() {
     var home = new Home();
-    home.streetAddress = streetAddress;
-    home.numBedrooms = numBedrooms;
-    home.numBathrooms = numBathrooms;
-    home.nearestStation = nearestStation;
-    home.walkTimeToStation = walkTimeToStation;
-    home.directCentralStation = directCentralStation;
-    home.timeToCentral = timeToCentral;
-    home.notes = notes;
+    home.streetAddress = "1600 Pennsylvania Ave";
+    home.numBedrooms = 100;
+    home.numBathrooms = 200;
+    home.nearestStation = "wdc";
+    home.walkTimeToStation = 20;
+    home.directCentralStation = "wdc";
+    home.timeToCentral = 0;
+    home.notes = "White House";
     return home;
   }
 }
